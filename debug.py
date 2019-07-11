eb6d262619c196895de25bf53d426ef1c3e13fc9
bamboo <chengzhengwu@aliyun.com>
2019-07-11 09:25:48 +0800

    'stack'

--- debug.py
+++ debug.py
@@ -1,122 +1,23 @@
-# class Solution(object):
-#     def numDecodings(self, s):
-#         """
-#         :type s: str
-#         :rtype: int
-#         """
-#         i, ans = 0, 0
-#         while i < len(s):
-#             if int(s[i:i+2]) > 26:
-#                 i += 2
-#             else:
-#                 i += 1
-#             ans += 1
-#         return ans
-# 
-# 
-# 
-#
-
-# from collections import Counter
-# class Solution(object):
-#     def minWindow(self, s, t):
-#         """
-#         :type s: str
-#         :type t: str
-#         :rtype: str
-#         """
-#         t_d = dict(Counter(t))
-#         s_d = t_d.copy()
-#         print(s_d)
-#         i, j = 0, 0
-#         min_str = s
-#         while i <= j and j < len(s):
-#             if s[j] in s_d:
-#                 s_d[s[j]] -= 1
-#                 if s_d[s[j]] <= 0:
-#                     s_d.pop(s[j])
-#             while not s_d and i < j:
-#                 min_str = min_str if min_str < s[i:j + 1] else s[i:j + 1]
-#                 if s[i] in t_d:
-#                     s_d[s[i]] = s_d.get(s[i], 0) + 1
-#                 i += 1
-#             j += 1
-#         return min_str
-# s = Solution()
-# print(s.minWindow(s = "ADOBECODEBANC", t = "ABC"))
-
-
-# from collections import Counter
-# class Solution(object):
-#     def minWindow(self, s, t):
-#         """
-#         :type s: str
-#         :type t: str
-#         :rtype: str
-#         """
-#         t_d = Counter(t)
-#         i, j = 0, 0
-#         missing = len(t)
-#         min_str = s
-#         while j < len(s):
-#             missing -= t_d[s[j]] > 0
-#             t_d[s[j]] -= 1
-#             while t_d[s[i]] < 0 or s[i] in :
-#                 min_str = min_str if min_str < s[i:j + 1] else s[i:j + 1]
-#                 t_d[s[i]] += 1
-#                 i += 1
-#             j += 1
-#         return min_str
-# s = Solution()
-# print(s.minWindow(s = "ADOBECODEBANC", t = "ABC"))
-
-# class Solution(object):
-#     def exist(self, board, word):
-#         """
-#         :type board: List[List[str]]
-#         :type word: str
-#         :rtype: bool
-#         """
-#         self.board = board
-#         self.word = word
-#         self.vis = [[False for _ in range(len(board[0]))] for _ in range(len(board))]
-#         self.direct = ((1,0), (0, 1), (0, -1), (-1, 0))
-#
-#     def dfs(self, i, j, index):
-#         if index >= len(self.word):
-#             return True
-#         if self.board[i][j] != self.word[index]:
-#             return False
-#         result = False
-#         for d in self.direct:
-#             if not self.vis[i+d[0]][j+d[1]]:
-#                 self.vis[i + d[0]][j + d[1]] = True
-#                 result = result or self.dfs(i+d[0], j+d[1], index+1)
-#         return result
-#
-#
-#
-
-
 class Solution(object):
-    def evalRPN(self, tokens):
+    def longestValidParentheses(self, s):
         """
-        :type tokens: List[str]
+        :type s: str
         :rtype: int
         """
-        stack = []
-        operater = {'+', '-', '*', '/'}
-        for c in tokens:
-            if c in operater:
-                end = stack.pop()
-                start = stack.pop()
-                stack.append(str(int(eval(start+c+end))))
-            else:
-                stack.append(c)
-        print(stack)
-        return int(stack[-1])
-
-
+        n = len(s)
+        dp = [[False for i in range(n+1)] for j in range(n)]
+        # (())()
+        max_par = -1
+        for i in range(n):
+            dp[i][i] = 0
+            for j in range(i+1, n+1):
+                if (j - i) % 2 == 0:
+                    if s[i] == '(' and s[j-1] == ')' and dp[i+1][j-1] is not False:
+                        dp[i][j] = dp[i+1][j-1] + 2
+                        max_par = max(dp[i][j], max_par)
+                    if j >=2 and s[j-2] == '(' and s[j-1] == ')' and dp[i][j-2] is not False:
+                        dp[i][j] = dp[i][j - 2] + 2
+                        max_par = max(dp[i][j], max_par)
+        return max_par
 s = Solution()
-print(s.evalRPN(
- ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]))
+print s.longestValidParentheses(")()(())())")

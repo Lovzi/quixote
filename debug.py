2f37deef74be2774aa59489be5abe26884e57f5a
Goook <chengzhengwu@aliyun.com>
2019-07-22 17:29:25 +0800

    coding

--- debug.py
+++ debug.py
@@ -25,20 +25,86 @@
 # s = Solution()
 # print(s.wordBreak("aaaaaaa", ["aaaa","aa","a"]))
 
+import math
+import math
+
 
 class Solution(object):
-    cache = {}
-    def integerReplacement(self, n):
+    def minFallingPathSum(self, A):
         """
-        :type n: int
+        :type A: List[List[int]]
         :rtype: int
         """
-        if n == 1:
+        self.array = A
+        self.row_len = len(A[0])
+        self.A_len = len(A)
+        self.cache = {}
+        min_ans = math.inf
+        for i in range(len(A[0])):
+            min_ans = min(min_ans, self.recursion(0, i, i))
+        return min_ans
+
+    def recursion(self, height, start, end):
+        if height >= self.A_len:
             return 0
-        if n in self.cache:
-            return self.cache[n]
-        if n % 2 != 0:
-            self.cache[n] = min(self.integerReplacement(n + 1), self.integerReplacement(n-1)) + 1
-        else:
-            self.cache[n] = self.integerReplacement(n // 2) + 1
-        return self.cache[n]
+        start = start if start > 0 else 0
+        end = end if end < self.row_len - 1 else self.row_len - 1
+        if (height, start, end) in self.cache:
+            return self.cache[(height, start, end)]
+        min_tmp = math.inf
+        for i in range(start, end + 1):
+            min_tmp = min(self.array[height][i] + self.recursion(height + 1, i - 1, i + 1), min_tmp)
+        self.cache[(height, start, end)] = min_tmp
+        return min_tmp
+
+s = Solution()
+print(s.minFallingPathSum([[1,2,3],[4,5,6],[7,8,9]]))
+
+
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        self.string = s
+        self.max_val = 0
+        self.s_len = len(s)
+        self.recursion(0, len(s)-1, {})
+        return self.max_val
+
+    def recursion(self, start, end, cache):
+        if start > end:
+            return True
+        if start < 0 or end >= self.s_len:
+            return False
+
+        if (start, end) in cache:
+            return cache[(start, end)]
+
+        cache[(start, end)] = False
+        if self.string[start] == ')':
+            self.recursion(start+1, end, cache)
+            return False
+        if self.string[end] == '(':
+            self.recursion(start, end-1, cache)
+            return False
+
+        if self.string[start] == '(' and self.string[end] == ')':
+            cache[(start, end)] = self.recursion(start+1, end-1, cache)
+            if cache[(start, end)]:
+                self.max_val = max(self.max_val, end-start+1)
+                return True
+
+
+        for i in range(start, end):
+            left = self.recursion(start, i, cache)
+            right = self.recursion(i+1, end, cache)
+            if  left and right:
+                cache[(start, end)] = True
+                self.max_val = max(self.max_val, end - start + 1)
+                return True
+        return False
+
+s = Solution()
+print(s.longestValidParentheses(')((()))'))
